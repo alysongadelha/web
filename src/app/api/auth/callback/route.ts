cfc49c991b7723c6a997bd37f4d764de08f337cb
Alyson Gadelha <alyson_sud@hotmail.com>
2023-05-22 13:52:05 +0100

    day 04

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,13 +5,18 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
-  const registerREsponse = await api.post("/register", {
+  const redirectTo = request.cookies.get("redirectTo")?.value;
+
+  const registerResponse = await api.post("/register", {
     code,
   });
 
-  const { token } = registerREsponse.data;
+  const { token } = registerResponse.data;
+
+  const redirectURL = redirectTo ?? new URL("/", request.url);
+
   const cookieExpiresInSeconds = 60 + 60 + 24 * 30;
-  const redirectURL = new URL("/", request.url);
+
   return NextResponse.redirect(redirectURL, {
     headers: {
       "Set-Cookie": `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
